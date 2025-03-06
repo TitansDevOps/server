74a015821313fd20299d712f42cb61949704e671
maicolaa <maicolaroyave10@gmail.com>
2025-03-05 21:53:46 -0500

    Inicio

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Retrieve App configuration
- */
-
-export const config = {
-  language: 'english',
-  URL_CLIENT: process.env.URL_CLIENT,
-};
--- src/modules/auth/constants/jwt.constant.ts
+++
@@ -1,4 +0,0 @@
-export const jwtConstants = {
-  secret: 'no utilizar esta palabra en producción',
-  expiresIn: '3h',
-};
