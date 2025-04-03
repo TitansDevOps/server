05ec9f5aa214e8c0f3c9d6043259db3fbb8f2e7a
maicolaa <maicolaroyave10@gmail.com>
2025-04-02 23:27:29 -0500

    fix: sonarQube fixed errors

--- src/modules/file/file/file.service.ts
+++ src/modules/file/file/file.service.ts
@@ -20,10 +20,13 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { join } from 'path';
 import * as mime from 'mime-types';
+import { randomUUID } from 'crypto';
 
 import { extname } from 'path';
 import { messages } from 'src/messages/messages';
 import { CreateFileEntityBase64Dto } from './dto/create-file-base64.dto';
+import { SaveFilesTransporter } from './utils/save-files-transporter';
+
 @Injectable()
 export class FileService {
   constructor(
@@ -47,49 +50,9 @@ export class FileService {
     files: Array<Express.Multer.File>,
     createFileEntityDto: CreateFileEntityDto,
   ) {
-    const fileEntityPublic = PublicFileEntity.find((fileEntity) => {
-      return fileEntity === createFileEntityDto.typeEntity;
-    });
-
-    const fileEntityPrivate = PrivateFileEntity.find((fileEntity) => {
-      return fileEntity === createFileEntityDto.typeEntity;
-    });
-
-    let filePathOrigin = `uploads/private/${createFileEntityDto.typeEntity.toLowerCase()}/`;
-    let isPublic = false;
-    if (fileEntityPublic) {
-      isPublic = true;
-      filePathOrigin = `uploads/public/${createFileEntityDto.typeEntity.toLowerCase()}/`;
-    }
-
-    if (!fileEntityPrivate && !isPublic) {
-      throw new BadRequestException(messages.errorFileEntityNotFound);
-    }
-
-    let fileEntity = await this.fileEntityRepository.findOne({
-      where: {
-        typeEntity: createFileEntityDto.typeEntity,
-        entityOwnerId: createFileEntityDto.entityOwnerId,
-      },
-    });
-
-    if (!fileEntity) {
-      fileEntity = this.fileEntityRepository.create(createFileEntityDto);
-      fileEntity = await this.fileEntityRepository.save(fileEntity);
-    }
-
+    const { fileEntity, uploadsDir, filePathOrigin } =
+      await this.initSaveFiles(createFileEntityDto);
     const savedFiles = [];
-    let uploadsDir = isPublic
-      ? path.join(process.cwd(), 'uploads/public/')
-      : path.join(process.cwd(), 'uploads/private/');
-    uploadsDir = path.join(
-      uploadsDir,
-      createFileEntityDto.typeEntity.toLowerCase(),
-    );
-
-    if (!fs.existsSync(uploadsDir)) {
-      fs.mkdirSync(uploadsDir, { recursive: true });
-    }
 
     for (const file of files) {
       const randomName = `${Date.now()}${extname(file.originalname)}`;
@@ -123,7 +86,67 @@ export class FileService {
    * @param createFileEntityDto
    */
   async saveFilesBase64(createFileEntityDto: CreateFileEntityBase64Dto) {
-    const { files, typeEntity } = createFileEntityDto;
+    const createFileEntityDtoOriginal = new CreateFileEntityDto();
+    createFileEntityDtoOriginal.typeEntity = createFileEntityDto.typeEntity;
+    createFileEntityDtoOriginal.entityOwnerId =
+      createFileEntityDto.entityOwnerId;
+
+    const { fileEntity, uploadsDir, filePathOrigin } = await this.initSaveFiles(
+      createFileEntityDtoOriginal,
+    );
+    const { files } = createFileEntityDto;
+    const savedFiles = [];
+
+    for (const file of files) {
+      if (!file.base64) continue;
+
+      const detectedMime = file.base64.match(/^data:([^;]+);base64,/);
+      const mimeType = detectedMime
+        ? detectedMime[1]
+        : mime.lookup(file.name) || 'application/octet-stream';
+
+      let fileExt = extname(file.name);
+      if (!fileExt || fileExt === '.bin') {
+        fileExt = mime.extension(mimeType)
+          ? `.${mime.extension(mimeType)}`
+          : '.bin';
+      }
+
+      const randomName = `${Date.now()}-${randomUUID()}${fileExt}`;
+      const filePath = join(uploadsDir, randomName);
+      const base64Data = file.base64.replace(/^data:[^;]+;base64,/, '');
+
+      fs.writeFileSync(filePath, Buffer.from(base64Data, 'base64'));
+
+      const fileInfo = {
+        name: file.name,
+        filename: randomName,
+        ext: fileExt.slice(1),
+        type: mimeType,
+        fileDate: new Date(),
+        size: Buffer.from(base64Data, 'base64').length,
+        filePath: filePathOrigin + randomName,
+      };
+
+      const savedFile = await this.fileRepository.save(
+        this.fileRepository.create(fileInfo),
+      );
+      savedFiles.push(savedFile);
+
+      const fileEntityOwner = this.fileEntityOwnerRepository.create({
+        idEntity: fileEntity.id,
+        idFile: savedFile.id,
+      });
+      await this.fileEntityOwnerRepository.save(fileEntityOwner);
+    }
+
+    return savedFiles;
+  }
+
+  async initSaveFiles(
+    createFileEntityDto: CreateFileEntityDto,
+  ): Promise<SaveFilesTransporter> {
+    const { typeEntity } = createFileEntityDto;
 
     const fileEntityPublic = PublicFileEntity.find((fileEntity) => {
       return fileEntity === createFileEntityDto.typeEntity;
@@ -165,52 +188,7 @@ export class FileService {
       fs.mkdirSync(uploadsDir, { recursive: true });
     }
 
-    const savedFiles = [];
-
-    for (const file of files) {
-      if (!file.base64) continue;
-
-      const detectedMime = file.base64.match(/^data:([^;]+);base64,/);
-      const mimeType = detectedMime
-        ? detectedMime[1]
-        : mime.lookup(file.name) || 'application/octet-stream';
-
-      let fileExt = extname(file.name);
-      if (!fileExt || fileExt === '.bin') {
-        fileExt = mime.extension(mimeType)
-          ? `.${mime.extension(mimeType)}`
-          : '.bin';
-      }
-
-      const randomName = `${Date.now()}-${Math.round(Math.random() * 1e9)}${fileExt}`;
-      const filePath = join(uploadsDir, randomName);
-      const base64Data = file.base64.replace(/^data:[^;]+;base64,/, '');
-
-      fs.writeFileSync(filePath, Buffer.from(base64Data, 'base64'));
-
-      const fileInfo = {
-        name: file.name,
-        filename: randomName,
-        ext: fileExt.slice(1),
-        type: mimeType,
-        fileDate: new Date(),
-        size: Buffer.from(base64Data, 'base64').length,
-        filePath: filePathOrigin + randomName,
-      };
-
-      const savedFile = await this.fileRepository.save(
-        this.fileRepository.create(fileInfo),
-      );
-      savedFiles.push(savedFile);
-
-      const fileEntityOwner = this.fileEntityOwnerRepository.create({
-        idEntity: fileEntity.id,
-        idFile: savedFile.id,
-      });
-      await this.fileEntityOwnerRepository.save(fileEntityOwner);
-    }
-
-    return savedFiles;
+    return new SaveFilesTransporter(fileEntity, uploadsDir, filePathOrigin);
   }
 
   /**
