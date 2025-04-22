e16718954f4a2a2633b08233801e68b28227342b
maicolaa <maicolaroyave10@gmail.com>
2025-04-21 21:25:06 -0500

    fix: vercel config

--- api/index.ts
+++ api/index.ts
@@ -1,44 +1,46 @@
-import 'tsconfig-paths/register';
-import { NowRequest, NowResponse } from '@vercel/node'; 
+// Configuración de aliases (SOLUCIÓN PRINCIPAL)
+import 'module-alias/register';
+require('module-alias').addAliases({
+  '@modules': __dirname + '/../src/modules'
+});
+
+// Imports esenciales
+import { VercelRequest, VercelResponse } from '@vercel/node';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { AllExceptionsFilter } from '../src/modules/admin/all-exceptions.filter';
+import { AllExceptionsFilter } from '@modules/admin/all-exceptions.filter'; // Usando el alias
 import { json, urlencoded } from 'body-parser';
 
-import { register } from 'tsconfig-paths';
-import { resolve } from 'path';
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  try {
+    const app = await NestFactory.create(AppModule);
 
-register({
-  baseUrl: __dirname,
-  paths: {
-    '@modules/*': [resolve(__dirname, '../src/modules/*')]
+    // Configuración de la aplicación
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+
+    app.enableCors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    });
+
+    app.useGlobalFilters(new AllExceptionsFilter());
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ extended: true, limit: '50mb' }));
+
+    await app.init();
+
+    const server = app.getHttpAdapter().getInstance();
+    server(req, res);
+  } catch (error) {
+    console.error('Error inicializando la aplicación:', error);
+    res.status(500).send('Internal Server Error');
   }
-});
-
-export default async function handler(req: NowRequest, res: NowResponse) {
-  const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
-
-  app.enableCors({
-    origin: '*', 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-
-  app.useGlobalFilters(new AllExceptionsFilter());
-  app.use(json({ limit: '50mb' }));
-  app.use(urlencoded({ extended: true, limit: '50mb' }));
-
-  await app.init();
-
-  const server = app.getHttpAdapter().getInstance();
-  server(req, res);
 }
